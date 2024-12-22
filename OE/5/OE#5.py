2cde00d14c6957a43f1ad531762bb4bb0c1a1e6a
Ivanmir-Cudal <cudal.ivanmir@gmail.com>
2024-12-22 16:34:15 +0800

    Add files via upload

---
+++ OE/5/OE#5.py
@@ -0,0 +1,17 @@
+NAME: Cudal, Ivanmir F.
+YR/SECTION: BSIT - 2A
+
+GAME NAME: BloxFruit 
+attributes:
+- Fruit(T-rex)
+- Mele
+- Gun
+- Sword
+- Race
+- Haki
+- observation Haki
+methods:
+- Z move
+- X move
+- C move
+- Q Dash
